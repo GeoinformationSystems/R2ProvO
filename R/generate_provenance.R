0d2adcb1c913762dc34c1b29f5ecc573a3ba1d8a
Arne Ruemmler <arne.ruemmler@tu-dresden.de>
2021-08-03 16:25:02 +0200

    refactor namespace

--- R/generate_provenance.R
+++ R/generate_provenance.R
@@ -10,7 +10,7 @@ init_prov <- function() {
         rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
         rdfs = "http://www.w3.org/2000/01/rdf-schema#",
         prov = "http://www.w3.org/ns/prov#",
-        rscript = "http://www.r.script#"
+        rscript = "http://www.r.rscript#"
     )
 }
 
@@ -22,12 +22,12 @@ prov <- function(expression) {
     output_string <- (toString(expression[[2]]))
     output_iri <- URLencode(output_string, reserved = TRUE)
     pkg.env$rdf %>% rdflib::rdf_add(
-        subject = paste0(pkg.env$namespaces["ex"], output_iri),
+        subject = paste0(pkg.env$namespaces["rscript"], output_iri),
         predicate = paste0(pkg.env$namespaces["rdf"], "type"),
         object = paste0(pkg.env$namespaces["prov"], "Entity")
     )
     pkg.env$rdf %>% rdflib::rdf_add(
-        subject = paste0(pkg.env$namespaces["ex"], output_iri),
+        subject = paste0(pkg.env$namespaces["rscript"], output_iri),
         predicate = paste0(pkg.env$namespaces["rdfs"], "label"),
         object = output_string
     )
@@ -40,43 +40,43 @@ prov <- function(expression) {
     process_iri <- URLencode(process_string, reserved = TRUE)
     process_instance <- paste(process_iri, uuid::UUIDgenerate(), sep = "_")
     pkg.env$rdf %>% rdflib::rdf_add(
-        subject = paste0(pkg.env$namespaces["ex"], process_instance),
+        subject = paste0(pkg.env$namespaces["rscript"], process_instance),
         predicate = paste0(pkg.env$namespaces["rdf"], "type"),
         object = paste0(pkg.env$namespaces["prov"], "Activity")
     )
     pkg.env$rdf %>% rdflib::rdf_add(
-        subject = paste0(pkg.env$namespaces["ex"], process_instance),
+        subject = paste0(pkg.env$namespaces["rscript"], process_instance),
         predicate = paste0(pkg.env$namespaces["rdfs"], "label"),
         object = process_string
     )
     pkg.env$rdf %>% rdflib::rdf_add(
-        subject = paste0(pkg.env$namespaces["ex"], output_iri),
+        subject = paste0(pkg.env$namespaces["rscript"], output_iri),
         predicate = paste0(pkg.env$namespaces["prov"], "wasGeneratedBy"),
-        object = paste0(pkg.env$namespaces["ex"], process_instance)
+        object = paste0(pkg.env$namespaces["rscript"], process_instance)
     )
 
     for (i in 2:length(right_side)) {
         current_input_string <- toString(right_side[[i]])
         current_input_iri <- URLencode(current_input_string, reserved = TRUE)
         pkg.env$rdf %>% rdflib::rdf_add(
-            subject = paste0(pkg.env$namespaces["ex"], current_input_iri),
+            subject = paste0(pkg.env$namespaces["rscript"], current_input_iri),
             predicate = paste0(pkg.env$namespaces["rdf"], "type"),
             object = paste0(pkg.env$namespaces["prov"], "Entity")
         )
         pkg.env$rdf %>% rdflib::rdf_add(
-            subject = paste0(pkg.env$namespaces["ex"], current_input_iri),
+            subject = paste0(pkg.env$namespaces["rscript"], current_input_iri),
             predicate = paste0(pkg.env$namespaces["rdfs"], "label"),
             object = current_input_string
         )
         pkg.env$rdf %>% rdflib::rdf_add(
-            subject = paste0(pkg.env$namespaces["ex"], process_instance),
+            subject = paste0(pkg.env$namespaces["rscript"], process_instance),
             predicate = paste0(pkg.env$namespaces["prov"], "used"),
-            object = paste0(pkg.env$namespaces["ex"], current_input_iri)
+            object = paste0(pkg.env$namespaces["rscript"], current_input_iri)
         )
         pkg.env$rdf %>% rdflib::rdf_add(
-            subject = paste0(pkg.env$namespaces["ex"], output_iri),
+            subject = paste0(pkg.env$namespaces["rscript"], output_iri),
             predicate = paste0(pkg.env$namespaces["prov"], "wasDerivedFrom"),
-            object = paste0(pkg.env$namespaces["ex"], current_input_iri)
+            object = paste0(pkg.env$namespaces["rscript"], current_input_iri)
         )
     }
 
